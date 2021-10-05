d3ff2f0269b12f3af86cd8235f29c74ceb331af0
Irvin Cruz <irvincruz912@gmail.com>
2021-10-05 10:17:31 -0500

    UPDATE: se actualiza dependencia de JUnit 5

---
+++ src/test/java/mx/santander/fiduciario/instruccionregistro/dto/instruction/send/req/SendInstrTypeInstructionDtoTest.java
@@ -0,0 +1,14 @@
+package mx.santander.fiduciario.instruccionregistro.dto.instruction.send.req;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SendInstrTypeInstructionDtoTest {
+
+	@Test
+	void test() {
+		assertEquals(true, true);
+	}
+
+}
